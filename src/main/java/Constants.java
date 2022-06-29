9620d487683a9900305daceb8e7e6a2f84d9bb84
Ido Cohen <idoc1996@gmail.com>
2022-06-29 22:26:53 +0300

    add constants and function

--- src/main/java/Constants.java
+++ src/main/java/Constants.java
@@ -1,12 +1,24 @@
 public class Constants {
 
-    public static final int WIN_WIDTH=1000,WIN_HEIGHT=600;
-    public static final String FONT_TYPE="Ariel";
-    public static final int SIZE_FONT_LABEL=18,FONT_SIZE_BUTTON=20;
-    public static final int TEXT_FIELD_WIDTH=300,TEXT_FIELD_HEIGHT=40;
-    public static final int TOP_MARGIN=15,RIGHT_MARGIN=15;
-    public static final String FACEBOOK_URL="https://www.facebook.com/";
-    public static final String INITIAL_DRIVER="webdriver.chrome.driver";
-    public static final String DRIVER_PATH="C:\\Users\\User\\Desktop\\לימודים עדו\\שנה א\\סמסטר ב\\סדנא לתכנות\\chromedriver_win32\\chromedriver.exe";
-    public static final String USER_NAME="AIUA1234@WALLA.CO.IL",PASSWORD="AIUA2469";
+    public static final int WIN_WIDTH = 1000, WIN_HEIGHT = 600;
+    public static final int SIZE_FONT_LABEL = 18, FONT_SIZE_BUTTON = 20;
+    public static final int TEXT_FIELD_WIDTH = 300, TEXT_FIELD_HEIGHT = 40;
+    public static final int TOP_MARGIN = 15, RIGHT_MARGIN = 15;
+    public static final String FACEBOOK_URL = "https://www.facebook.com/";
+    public static final String INITIAL_DRIVER = "webdriver.chrome.driver";
+    public static final String DRIVER_PATH = "C:\\Users\\User\\Desktop\\לימודים עדו\\שנה א\\סמסטר ב\\סדנא לתכנות\\chromedriver_win32\\chromedriver.exe";
+    public static final String USER_NAME = "AIUA1234@WALLA.CO.IL", PASSWORD = "AIUA2469";
+    public static final String FONT_TYPE = "Ariel";
+    public static final String WINDOW_TITLE = "image processing";
+    public static final String BACKGROUND_NAME = "imageProcess.png";
+    public static final String AFTER_TITLE = "After", BEFORE_TITLE = "Before";
+    public static final String SEARCH_BUTTON ="search";
+    public static final String FILTER_COLOR_SHIFT_LEFT="Color shift Left"
+                                ,FILTER_COLOR_SHIFT_RIGHT= "Color shift Right",
+                                FILTER_NEGATIVE_COLOR ="Negative color",
+                                FILTER_GRAY_SCALE ="Gray scale",
+                                FILTER_LIGHTER = "lighter",
+                                FILTER_MIRROR="Mirror";
+
+
 }
--- src/main/java/ProcessingScene.java
+++ src/main/java/ProcessingScene.java
@@ -1,3 +1,5 @@
+import org.checkerframework.checker.units.qual.C;
+
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
@@ -14,10 +16,11 @@ public class ProcessingScene extends JPanel {
     private BufferedImage bufferedImage;
     private ImageIcon backGround;
 
+
     public ProcessingScene(int x, int y, int width, int height) {
         this.setLayout(null);
         this.setBounds(x, y, width, height);
-        this.backGround=new ImageIcon("imageProcess.png");
+        this.backGround = new ImageIcon(Constants.BACKGROUND_NAME);
         mainView();
         this.setDoubleBuffered(true);
         this.setVisible(true);
@@ -25,11 +28,11 @@ public class ProcessingScene extends JPanel {
 
     private void mainView() {
         this.textField = Helper.addTextField(this, this.getWidth() / 2 - Constants.TEXT_FIELD_WIDTH / 2, Constants.TOP_MARGIN, Constants.TEXT_FIELD_WIDTH, Constants.TEXT_FIELD_HEIGHT);
-        this.searchButton = Helper.addButton(this, "Get Image", this.textField.getX(), this.textField.getY() + this.textField.getHeight() + Constants.TOP_MARGIN, this.textField.getWidth(), this.textField.getHeight());
+        this.searchButton = Helper.addButton(this, Constants.SEARCH_BUTTON, this.textField.getX(), this.textField.getY() + this.textField.getHeight() + Constants.TOP_MARGIN, this.textField.getWidth(), this.textField.getHeight());
         this.searchButton.addActionListener((e) -> {
-            ImageUrl imageUrl=new ImageUrl(this.textField.getText());
-            String url=imageUrl.getImage(imageUrl.getProfileName());
-            if (this.picture!=null){
+            ImageUrl imageUrl = new ImageUrl(this.textField.getText());
+            String url = imageUrl.getImage(imageUrl.getProfileName());
+            if (this.picture != null) {
                 this.remove(picture);
                 this.remove(processPicture);
                 repaint();
@@ -42,7 +45,7 @@ public class ProcessingScene extends JPanel {
     private void processView(String urlLink) {
         try {
             this.url = ImageTransferring.getUrl(urlLink);
-            this.bufferedImage= ImageIO.read(this.url);
+            this.bufferedImage = ImageIO.read(this.url);
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -56,22 +59,24 @@ public class ProcessingScene extends JPanel {
         addTittleLoading();
         repaint();
     }
-    private void addTittlePic(){
-        Helper.addLabel(this,"After",  (int) (this.picture.getWidth()/2.5),this.picture.getHeight()+15,100,50);
-        Helper.addLabel(this,"Before", (int) (this.getWidth() - this.picture.getWidth()/1.5),this.picture.getHeight()+15,100,50);
+
+    private void addTittlePic() {
+        Helper.addLabel(this, Constants.AFTER_TITLE, (int) (this.picture.getWidth() / 2.5), this.picture.getHeight() + 15, 100, 50);
+        Helper.addLabel(this, Constants.BEFORE_TITLE, (int) (this.getWidth() - this.picture.getWidth() / 1.5), this.picture.getHeight() + 15, 100, 50);
     }
-    private void addTittleLoading(){
-        Helper.addLabel(this,"Loading Please wait...",this.searchButton.getX()+50,Constants.WIN_HEIGHT-150,this.picture.getHeight()+15,50);
+
+    private void addTittleLoading() {
+        Helper.addLabel(this, "Loading Please wait...", this.searchButton.getX() + 50, Constants.WIN_HEIGHT - 150, this.picture.getHeight() + 15, 50);
     }
 
     private void addProcessButtons() {
         JButton[] buttons = new JButton[6];
-        buttons[0] = Helper.addButton(this, "Color shift Left", this.searchButton.getX(), this.searchButton.getY() + this.searchButton.getHeight() + Constants.TOP_MARGIN, Constants.TEXT_FIELD_WIDTH, Constants.TEXT_FIELD_HEIGHT);
-        buttons[1] = Helper.addButton(this, "Color shift Right", this.searchButton.getX(), buttons[0].getY() + buttons[0].getHeight() + Constants.TOP_MARGIN, Constants.TEXT_FIELD_WIDTH, Constants.TEXT_FIELD_HEIGHT);
-        buttons[2] = Helper.addButton(this, "Negative color", this.searchButton.getX(), buttons[1].getY() + buttons[1].getHeight() + Constants.TOP_MARGIN, Constants.TEXT_FIELD_WIDTH, Constants.TEXT_FIELD_HEIGHT);
-        buttons[3] = Helper.addButton(this, "Gray scale", this.searchButton.getX(), buttons[2].getY() + buttons[2].getHeight() + Constants.TOP_MARGIN, Constants.TEXT_FIELD_WIDTH, Constants.TEXT_FIELD_HEIGHT);
-        buttons[4] = Helper.addButton(this, "Lighter", this.searchButton.getX(), buttons[3].getY() + buttons[3].getHeight() + Constants.TOP_MARGIN, Constants.TEXT_FIELD_WIDTH, Constants.TEXT_FIELD_HEIGHT);
-        buttons[5] = Helper.addButton(this, "Mirror", this.searchButton.getX(), buttons[4].getY() + buttons[4].getHeight() + Constants.TOP_MARGIN, Constants.TEXT_FIELD_WIDTH, Constants.TEXT_FIELD_HEIGHT);
+        buttons[0] = Helper.addButton(this, Constants.FILTER_COLOR_SHIFT_LEFT, this.searchButton.getX(), this.searchButton.getY() + this.searchButton.getHeight() + Constants.TOP_MARGIN, Constants.TEXT_FIELD_WIDTH, Constants.TEXT_FIELD_HEIGHT);
+        buttons[1] = Helper.addButton(this, Constants.FILTER_COLOR_SHIFT_RIGHT, this.searchButton.getX(), buttons[0].getY() + buttons[0].getHeight() + Constants.TOP_MARGIN, Constants.TEXT_FIELD_WIDTH, Constants.TEXT_FIELD_HEIGHT);
+        buttons[2] = Helper.addButton(this, Constants.FILTER_NEGATIVE_COLOR, this.searchButton.getX(), buttons[1].getY() + buttons[1].getHeight() + Constants.TOP_MARGIN, Constants.TEXT_FIELD_WIDTH, Constants.TEXT_FIELD_HEIGHT);
+        buttons[3] = Helper.addButton(this, Constants.FILTER_GRAY_SCALE, this.searchButton.getX(), buttons[2].getY() + buttons[2].getHeight() + Constants.TOP_MARGIN, Constants.TEXT_FIELD_WIDTH, Constants.TEXT_FIELD_HEIGHT);
+        buttons[4] = Helper.addButton(this, Constants.FILTER_LIGHTER, this.searchButton.getX(), buttons[3].getY() + buttons[3].getHeight() + Constants.TOP_MARGIN, Constants.TEXT_FIELD_WIDTH, Constants.TEXT_FIELD_HEIGHT);
+        buttons[5] = Helper.addButton(this, Constants.FILTER_MIRROR, this.searchButton.getX(), buttons[4].getY() + buttons[4].getHeight() + Constants.TOP_MARGIN, Constants.TEXT_FIELD_WIDTH, Constants.TEXT_FIELD_HEIGHT);
 
         buttons[0].addActionListener((e) -> {
 
@@ -93,20 +98,22 @@ public class ProcessingScene extends JPanel {
 
         });
         buttons[4].addActionListener((e) -> {
-           changeImage(ImageProcessing.lighter(this.bufferedImage));
+            changeImage(ImageProcessing.lighter(this.bufferedImage));
         });
         buttons[5].addActionListener((e) -> {
             changeImage(ImageProcessing.mirror(this.bufferedImage));
         });
 
     }
-    private void changeImage(JLabel newLabel){
+
+    private void changeImage(JLabel newLabel) {
         this.remove(this.processPicture);
         this.processPicture = newLabel;
         this.processPicture.setBounds(this.getWidth() - this.picture.getWidth() - Constants.RIGHT_MARGIN, 0, this.picture.getWidth(), this.picture.getHeight());
         this.add(this.processPicture);
         repaint();
     }
+
     public void paintComponent(Graphics graphics) {
         super.paintComponent(graphics);
         graphics.drawImage(this.backGround.getImage(), this.getX(), this.getY(), this.getWidth(), this.getHeight(), null);
